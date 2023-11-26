2f81a40f7323e055771a6c809099a51dfbf8ae08
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-27 00:37:10 +0530

    changes

--- src/comp/Message.jsx
+++ src/comp/Message.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Message = () => {
   return (
     <div className='message'>
-       <div className="message-container">
+       <div className="message-container owner">
           <div className="message-info">
             <img src="https://images.pexels.com/photos/1642228/pexels-photo-1642228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
             <span>just now</span>
           </div>
           <div className="message-content">
             <p>hello</p>
-            {/* <img src="https://images.pexels.com/photos/1642228/pexels-photo-1642228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /> */}
+            <img src="https://images.pexels.com/photos/1642228/pexels-photo-1642228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
           </div>
        </div>
     </div>
