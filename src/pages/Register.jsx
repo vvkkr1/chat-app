3a1993bbf27f17d2ed257da12996b998b88993f0
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-27 13:36:45 +0530

    async function is wrapped with await keyword instead .then() and .catch()

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import Add from '../img/addAvatar.png'
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {auth} from "../firebase/firebase";
+
 function Register(){
-  function handleSubmit(e){
+  
+  async function handleSubmit(e){
     //to overcome refresh
     e.preventDefault();
+
     // console.log(e.target[0].value) total - 4 targets with its index
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     //if you are using file mention how many attachments you need
     const file = e.target[3].files[0]
+
+//Promises used here
+    //here is the main part
+      // const auth = getAuth();
+      try{
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(res)
+      }
+      catch(error){
+        console.log("Error!!!",error)
+      }
+        // .then((userCredential) => {
+        //   // Signed up 
+        //   const user = userCredential.user;
+        //   console.log("__________",user)
+        //   // ...
+        // })
+        // .catch((error) => {
+        //   const errorCode = error.code;
+        //   const errorMessage = error.message;
+        //   // ..
+        // });
   }
 
   return (
